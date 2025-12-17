449439c30d14cdb7b191fc777693950d8597c53c
Krish Grover <gkrish088@gmail.com>
2025-12-18 03:15:53 +0530

    lesson 3 : ConversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,72 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+// both of the upper syntax are valid to know datatype of variable
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // this will show that the datatype is number
+console.log(valueInNumber); // this will show NaN because hitesh cannot be converted to number
+
+// Conversion to Number
+// "hitesh" => NaN
+
+// below are some examples of conversion to number
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber) // this will convert number to string
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
