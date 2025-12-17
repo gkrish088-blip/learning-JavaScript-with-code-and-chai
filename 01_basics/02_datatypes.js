90e3bd4335abc57928a87a30a86af7e5fc99a0df
Krish Grover <gkrish088@gmail.com>
2025-12-18 02:57:47 +0530

    lesson 2 - Data Types in JavaScript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser it works in browser only
+
+console.log(3 
+    +
+     3) // code readability should be high, this type of formatting should be avoided
+
+ console.log("Hitesh") // ; is optional in js.
+
+
+let name = "hitesh" // string
+let age = 18 // number
+let isLoggedIn = false // boolean
+let state = null
+let country = undefined
+let pinCode; // undefined
+
+console.log(typeof name) // to know datatype of variable
+
+// number => 2 to power 53
+// bigint => larger than 2 to power 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => means value is not assigned
+// symbol => used to identify something uniquely mostly in html and css , expecially in figma .
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
